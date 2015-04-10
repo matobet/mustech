bddc2444de10d86f4c3ee3a8a7ab15575c65af35
Martin Betak <mbetak@redhat.com>
2015-04-10 23:22:11 +0200

    frontend: Add ember-cli integration
    Added scaffolding for frontend project via ember-cli & frontend-maven-plugin.

--- src/main/java/cz/muni/fi/pv243/mustech/HelloApplication.java
+++ src/main/java/cz/muni/fi/pv243/mustech/HelloApplication.java
@@ -3,6 +3,6 @@ package cz.muni.fi.pv243.mustech;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/api")
 public class HelloApplication extends Application {
 }
