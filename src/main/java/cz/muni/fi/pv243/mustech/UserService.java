daac129f53c0dc90d57e221844dca45314e84d96
Milan <milan.panik1@gmail.com>
2015-04-28 01:11:45 +0200

    removed picketlink idm

--- src/main/java/cz/muni/fi/pv243/mustech/UserService.java
+++ src/main/java/cz/muni/fi/pv243/mustech/UserService.java
@@ -1,9 +1,6 @@
 package cz.muni.fi.pv243.mustech;
 
 import cz.muni.fi.pv243.mustech.dal.UserRepository;
-import org.picketlink.idm.IdentityManager;
-import org.picketlink.idm.PartitionManager;
-import org.picketlink.idm.credential.Password;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -16,8 +13,6 @@ public class UserService {
 
     @Inject
     private UserRepository userRepository;
-    @Inject
-    private PartitionManager partitionManager;
 
     @PostConstruct
     private void initUsers() {
@@ -25,13 +20,5 @@ public class UserService {
         user.setId(1);
         user.setName("Franta");
         userRepository.save(user);
-
-        IdentityManager identityManager = this.partitionManager.createIdentityManager();
-        org.picketlink.idm.model.basic.User admin = new org.picketlink.idm.model.basic.User("test");
-        admin.setFirstName("test");
-        admin.setLastName("Administrator");
-        identityManager.add(admin);
-        Password password = new Password("12345678");
-        identityManager.updateCredential(admin, password);
     }
 }
