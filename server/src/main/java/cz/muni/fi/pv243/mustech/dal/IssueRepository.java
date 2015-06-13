92675a9b05c9836e43589a1baf7be9cee5000ae0
Milan <milan.panik1@gmail.com>
2015-06-13 16:46:44 +0200

    added preloaded issue created by user@user.cz

--- server/src/main/java/cz/muni/fi/pv243/mustech/dal/IssueRepository.java
+++ server/src/main/java/cz/muni/fi/pv243/mustech/dal/IssueRepository.java
@@ -5,9 +5,9 @@ import org.apache.deltaspike.data.api.EntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
 /**
- * Created by Tomas on 26. 5. 2015.
+ * @author Tomas
  */
 @Repository
-public interface IssueRepository extends EntityRepository<Issue, Long>
-{
+public interface IssueRepository extends EntityRepository<Issue, Long> {
+
 }
--- server/src/main/java/cz/muni/fi/pv243/mustech/util/AppInitializer.java
+++ server/src/main/java/cz/muni/fi/pv243/mustech/util/AppInitializer.java
@@ -1,13 +1,16 @@
 package cz.muni.fi.pv243.mustech.util;
 
+import cz.muni.fi.pv243.mustech.model.Issue;
 import cz.muni.fi.pv243.mustech.model.RoleType;
 import cz.muni.fi.pv243.mustech.model.User;
+import cz.muni.fi.pv243.mustech.service.IssueService;
 import cz.muni.fi.pv243.mustech.service.UserService;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import java.util.Date;
 
 
 @Startup
@@ -16,6 +19,8 @@ public class AppInitializer {
 
     @Inject
     private UserService userService;
+    @Inject
+    private IssueService issueService;
 
     @PostConstruct
     private void initUsers() {
@@ -33,5 +38,17 @@ public class AppInitializer {
 
         userService.saveOrUpdate(admin);
         userService.saveOrUpdate(user);
+
+        initDemoIssues();
+    }
+
+    private void initDemoIssues() {
+        Issue dinner = new Issue();
+        dinner.setName("Dinner");
+        dinner.setDescription("Where should we go eat?");
+        dinner.setCreatedAt(new Date());
+        dinner.setCreatedBy(userService.findByEmail("user@user.cz"));
+
+        issueService.saveOrUpdate(dinner);
     }
 }
