84ef82a364d28a128bba8791d1e87e7af781af29
Milan <milan.panik1@gmail.com>
2015-06-13 16:47:24 +0200

    login now returns logged entity

--- server/src/main/java/cz/muni/fi/pv243/mustech/rest/LoginEndpoint.java
+++ server/src/main/java/cz/muni/fi/pv243/mustech/rest/LoginEndpoint.java
@@ -1,14 +1,17 @@
 package cz.muni.fi.pv243.mustech.rest;
 
+import cz.muni.fi.pv243.mustech.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -22,11 +25,15 @@ public class LoginEndpoint {
 
     private final Logger log = LoggerFactory.getLogger(LoginEndpoint.class);
 
+    @Inject
+    private UserService userService;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response login(@Context HttpServletRequest req, @Context HttpServletResponse response) throws IOException, ServletException {
         boolean authenticated = req.authenticate(response);
         log.info("logging in: " + req.getUserPrincipal() + " with success: " + authenticated);
-        return Response.status(Response.Status.OK).build();
+        return Response.status(Response.Status.OK).entity(userService.findByEmail(req.getUserPrincipal().getName())).build();
     }
 }
