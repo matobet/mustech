c35a78973eb6fe0707e31aed853410e43885184f
Milan <milan.panik1@gmail.com>
2015-06-19 21:40:02 +0200

    small tweak

--- frontend/app/controllers/issues/new.js
+++ frontend/app/controllers/issues/new.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save() {
-      let record = this.store.createRecord('issue', this.getProperties('name', 'description'));
+      let record = this.store.createRecord('issue', this.getProperties('name', 'description', 'expiresAt'));
       record.save()
         .then(() => this.notify.info('Issue created!'))
         .catch(e => {
