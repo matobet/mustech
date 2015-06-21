7597c33fa6a63b7cb46ded1f00ef7e617078c647
Milan <milan.panik1@gmail.com>
2015-06-21 17:22:17 +0200

    user can now create poll with dynamic options during issue creation

--- frontend/app/controllers/issues/new.js
+++ frontend/app/controllers/issues/new.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  options: [],
   actions: {
+    addPollOption() {
+      let options = this.get('options');
+      options.pushObject({
+        label: 'Option'
+      });
+    },
+    removePollOption(option) {
+      this.get('options').removeObject(option);
+    },
     save() {
-      let record = this.store.createRecord('issue', this.getProperties('name', 'description', 'expiresAt'));
-      record.save()
+      //TODO: save polls with issue
+      let issue = this.store.createRecord('issue', this.getProperties('name', 'description', 'expiresAt'));
+      issue.save()
         .then(() => this.notify.info('Issue created!'))
         .catch(e => {
           Ember.Logger.debug('Issue creation failed!', e);
